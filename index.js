89b598c7e434fc8ff572e6882c54a0c44280bbf6
Chinnu Reddy <chinnureddy010@gmail.com>
2025-03-04 12:05:06 +0530

    this is from chinnu

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log('Happy developing ✨')
-console.log("etywruyq")
+function addNumbers() {
+    var num1 = 10;
+    var num2 = 20;
+    sum = num1 + num2;
+    console.log(sum);
+}
+
+addNumbers();
+
+console.log("These chnages from chinnu");
